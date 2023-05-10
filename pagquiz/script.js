37ac5c2b8f6ffbdb667bb58257549f9f305065c4
Marcelo Torrieli <62179343+marcelinhonet@users.noreply.github.com>
2023-05-10 20:36:26 -0300

    Update script.js

--- pagquiz/script.js
+++ pagquiz/script.js
@@ -82,7 +82,7 @@ function answerTest() {
   document.getElementsByClassName("diploma")[0].style.display = "block";
   document.getElementsByClassName("diploma2")[0].style.display = "block";
  
-  if (resultado >= 6){
+  if (resultado >= 0){
   document.getElementById("nota_diploma").textContent = `Nota da avaliação: Você acertou ${resultado} de 10 questões!!!`;
   }
 
